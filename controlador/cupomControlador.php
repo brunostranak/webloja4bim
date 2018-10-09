ac946de5af850aef3bbd1321d66a0be7959c3be7
1731378 <1731378@LABBMAQ06.itp.local>
2018-10-09 12:19:50 -0300

    coomit da fernanda (DESCONTO)

--- controlador/cupomControlador.php
+++ controlador/cupomControlador.php
@@ -1,5 +1,28 @@
 <?php
 
+/** admin */
 
+/*function desconto(){
+
+	if (ehPost()) {
+        extract($_POST);
+        
+$valorP = 
+$percentual = 30;
+$valorcomDesconto = $valorP * ( 1 - $percentual / 100 );
+    
+	
+
+}
+
+/** admin */
+
+
+function listar(){
+	$dados["categorias"]= pegarTodasCategorias ();
+	exibir('produto/listar', $dados);
+
+}
+\\
 
 ?>
