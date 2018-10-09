f2a47e6de2cc6929baf357d6bbb675e0ddc453c1
1731343 <1731343@LABBMAQ13.itp.local>
2018-10-09 12:14:10 -0300

    carrinho

--- controlador/carrinhoControlador.php
+++ controlador/carrinhoControlador.php
@@ -1,29 +1,60 @@
 <?php
+require "modelo/produtoModelo.php";
+ 
+//http://localhost/app/carrinho
+function index()
+{
+    if (isset($_SESSION["carrinho"])) {
+        $produtosCarrinho = array();
+        foreach ($_SESSION["carrinho"] as $produtoID) {
+            $produtosCarrinho[] = pegarProdutoPorId($produtoID["id"]);
+        }
 
-	require "modelo/produtoModelo.php";
-	require "./servicos/carrinhoServico.php";
-	
-	
+        $dados["produtos"] = $produtosCarrinho;
+        exibir("carrinho/listar", $dados);
+    } else {
+        echo "Nao existem produtos no carrinho!";
+    }
+}
+
+//http://localhost/app/carrinho/adicionar/2
+function adicionar($id)
+{
+    if (!isset($_SESSION["carrinho"])) {
+        $_SESSION["carrinho"] = array();
+    }
+
+    //vai existir a sessao carrinho!
+    $alt = false ;
 
 
-	function index(){
-		
-		$carrinhoProdutos = $_SESSION["carrinho"];
-		$dados["produtos"] = pegarVariosProdutosPorId($carrinhoProdutos);
+    for ($i=0; $i < count($_SESSION["carrinho"]); $i++) {
+        if ($_SESSION["carrinho"][$i]["id"] == $id) {
+            $alt = true;
+            $_SESSION["carrinho"][$i]["quantidade"]++;
+        }
+    }
+    if (!$alt) {
+        $produto["id"] = $id;
 
-		exibir("produto/carrinho",$dados);
-	}
-	
-	function adicionar($id){
+        $produto["quantidade"]=1;
 
-		addCarrinho($id);
-		redirecionar("carrinho/index");
-	}
-	
-	function deletar($id){
-		unset($_SESSION["carrinho"][$id]);
-		unset($_SESSION["quant"]);
-		$_SESSION["carrinho"] = array_values($_SESSION["carrinho"]);
-		redirecionar("carrinho/index");
-	}
+        $_SESSION["carrinho"][] = $produto;
+    }
+
+ 
+   
+    redirecionar("carrinho");
+}
+
+//http://localhost/app/carrinho/deletar/2
+function deletar($index)
+{
+    unset($_SESSION["carrinho"][$index]);
+    $_SESSION["carrinho"] = array_values($_SESSION["carrinho"]);
+    redirecionar("carrinho/index");
+}
+
+
+
 ?>
