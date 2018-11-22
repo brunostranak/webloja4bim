d7fe851a80293747477c91c217cff6feeaaec4d6
1731378 <1731378@LABBMAQ06.itp.local>
2018-11-22 12:09:00 -0200

    vai percorrer o txt e fazer a conexão

--- bibliotecas/mysqli.php
+++ bibliotecas/mysqli.php
@@ -1,7 +1,12 @@
 <?php
-
+require_once ("./servicos/manipulacao.php");
 function conn() {
-    $cnx = mysqli_connect("localhost", "root", "", "webloja");
+    $dados = manipulacao();
+//print_r($dados);
+    
+    $cnx = mysqli_connect($dados[0], $dados[1], $dados[2], $dados[3]);
     if (!$cnx) die('Deu errado a conexao!');
+    
+    mysqli_set_charset ($cnx, "utf-8");
     return $cnx;
 }
