93d7d7f94f7532dab2e1aeef8a79ac22d616c61a
brunostranak <41579737+brunostranak@users.noreply.github.com>
2018-11-24 17:53:46 -0200

    Update uteis.php

--- bibliotecas/uteis.php
+++ bibliotecas/uteis.php
@@ -4,7 +4,7 @@ function ehPost() {
     $ehPost = $_SERVER["REQUEST_METHOD"] == "POST";
 	if($ehPost) {
 		foreach ($_POST as $key => $value) {
-			$_POST[$key] = mysqli_real_escape_string(conexao(),$value);
+			$_POST[$key] = mysqli_real_escape_string(conn(),$value);
 		}
 	}
     return $ehPost;
